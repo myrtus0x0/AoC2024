60d48022733307e30ecccb1c607641621e095d82
Myrtus <myrtus0x0@protonmail.com>
2024-12-24 12:29:24 -0800

    day11 cleanup

--- day11/solution.cpp
+++ day11/solution.cpp
@@ -1,9 +1,9 @@
 #include "solution.hpp"
 #include <cstdint>
 #include <iostream>
-#include <map>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 
 const int BLINKS = 25;
 const int SECOND_BLINKS = 75;
@@ -11,12 +11,7 @@ const int SECOND_BLINKS = 75;
 uint countDigits(uint64_t n) { return floor(log10(n)) + 1; }
 
 void splitNumber(uint64_t n, uint digits, uint64_t &left, uint64_t &right) {
-    int splitPoint = digits / 2;
-    uint64_t divisor = 1;
-    for (int i = 0; i < digits / 2; i++) {
-        divisor *= 10;
-    }
-
+    uint64_t divisor = pow(10, digits / 2);
     right = n % divisor;
     left = n / divisor;
 }
@@ -38,39 +33,31 @@ std::vector<uint64_t> transform(uint64_t stoneNumber) {
     }
 }
 
-void enactBlick(std::map<uint64_t, uint64_t> &stones) {
-    std::map<uint64_t, uint64_t> copiedStones = stones;
-
-    // iterate over a copy of the map to avoid modifying the map while iterating
-    for (const auto &stone : copiedStones) {
+void enactBlick(std::unordered_map<uint64_t, size_t> &stones) {
+    std::unordered_map<uint64_t, size_t> newStones;
+    for (const auto &stone : stones) {
         uint64_t val = stone.first;
         uint64_t count = stone.second;
 
-        auto newStones = transform(val);
-        stones[val] -= count;
-
-        for (const auto &newStone : newStones) {
-            if (stones.find(newStone) == stones.end()) {
-                stones[newStone] = 0;
-            }
-
-            stones[newStone] += count;
+        auto transformedStones = transform(val);
+        for (const auto &newStone : transformedStones) {
+            newStones[newStone] += count;
         }
     }
+
+    // basically everything in the old list can be ignored as the rules
+    // guarantee every stone is modified
+    stones = std::move(newStones);
 }
 
 int Solution::solvePart1(const std::vector<std::string> &input) {
-    std::map<uint64_t, uint64_t> stones;
+    std::unordered_map<uint64_t, size_t> stones;
 
     std::string newInput = input[0];
     std::stringstream ss(newInput);
 
     int num;
     while (ss >> num) {
-        if (stones.find(num) == stones.end()) {
-            stones[num] = 0;
-        }
-
         stones[num]++;
     }
 
@@ -87,17 +74,13 @@ int Solution::solvePart1(const std::vector<std::string> &input) {
 }
 
 uint64_t Solution::solvePart2(const std::vector<std::string> &input) {
-    std::map<uint64_t, uint64_t> stones;
+    std::unordered_map<uint64_t, size_t> stones;
 
     std::string newInput = input[0];
     std::stringstream ss(newInput);
 
     int num;
     while (ss >> num) {
-        if (stones.find(num) == stones.end()) {
-            stones[num] = 0;
-        }
-
         stones[num]++;
     }
 
